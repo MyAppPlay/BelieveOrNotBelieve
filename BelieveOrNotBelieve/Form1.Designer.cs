18688ea8062e295e741dab9420b656cc9bf9e83c
MyAppPlay <Tehmaser@gmail.com>
2019-06-24 19:29:12 +0300

    Добавьте файлы проекта.

---
+++ BelieveOrNotBelieve/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace BelieveOrNotBelieve
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tboxQuestion = new System.Windows.Forms.TextBox();
+            this.btDob = new System.Windows.Forms.Button();
+            this.btSave = new System.Windows.Forms.Button();
+            this.btUdal = new System.Windows.Forms.Button();
+            this.nudNumber = new System.Windows.Forms.NumericUpDown();
+            this.cboxTrue = new System.Windows.Forms.CheckBox();
+            this.оПрограммеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumber)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.оПрограммеToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1020, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            this.fileToolStripMenuItem.Click += new System.EventHandler(this.FileToolStripMenuItem_Click);
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.newToolStripMenuItem.Text = "New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.NewToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.OpenToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.SaveToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
+            // 
+            // tboxQuestion
+            // 
+            this.tboxQuestion.BackColor = System.Drawing.SystemColors.Info;
+            this.tboxQuestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tboxQuestion.Location = new System.Drawing.Point(174, 39);
+            this.tboxQuestion.Multiline = true;
+            this.tboxQuestion.Name = "tboxQuestion";
+            this.tboxQuestion.Size = new System.Drawing.Size(674, 108);
+            this.tboxQuestion.TabIndex = 1;
+            this.tboxQuestion.TextChanged += new System.EventHandler(this.TBPlayPole_TextChanged);
+            // 
+            // btDob
+            // 
+            this.btDob.BackColor = System.Drawing.SystemColors.Info;
+            this.btDob.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btDob.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btDob.Location = new System.Drawing.Point(12, 273);
+            this.btDob.Name = "btDob";
+            this.btDob.Size = new System.Drawing.Size(133, 35);
+            this.btDob.TabIndex = 2;
+            this.btDob.Text = "Добавить";
+            this.btDob.UseVisualStyleBackColor = false;
+            this.btDob.Click += new System.EventHandler(this.BtDob_Click);
+            // 
+            // btSave
+            // 
+            this.btSave.BackColor = System.Drawing.SystemColors.Info;
+            this.btSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btSave.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.btSave.Location = new System.Drawing.Point(457, 271);
+            this.btSave.Name = "btSave";
+            this.btSave.Size = new System.Drawing.Size(129, 35);
+            this.btSave.TabIndex = 4;
+            this.btSave.Text = "Сохранить";
+            this.btSave.UseVisualStyleBackColor = false;
+            this.btSave.Click += new System.EventHandler(this.BtSave_Click);
+            // 
+            // btUdal
+            // 
+            this.btUdal.BackColor = System.Drawing.SystemColors.Info;
+            this.btUdal.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btUdal.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.btUdal.Location = new System.Drawing.Point(211, 271);
+            this.btUdal.Name = "btUdal";
+            this.btUdal.Size = new System.Drawing.Size(127, 35);
+            this.btUdal.TabIndex = 5;
+            this.btUdal.Text = "Удалить";
+            this.btUdal.UseVisualStyleBackColor = false;
+            this.btUdal.Click += new System.EventHandler(this.BtUdal_Click);
+            // 
+            // nudNumber
+            // 
+            this.nudNumber.BackColor = System.Drawing.SystemColors.Info;
+            this.nudNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nudNumber.Location = new System.Drawing.Point(742, 273);
+            this.nudNumber.Name = "nudNumber";
+            this.nudNumber.Size = new System.Drawing.Size(44, 35);
+            this.nudNumber.TabIndex = 6;
+            this.nudNumber.ValueChanged += new System.EventHandler(this.NumericUpDown1_ValueChanged);
+            // 
+            // cboxTrue
+            // 
+            this.cboxTrue.AutoSize = true;
+            this.cboxTrue.BackColor = System.Drawing.SystemColors.Info;
+            this.cboxTrue.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboxTrue.Location = new System.Drawing.Point(844, 271);
+            this.cboxTrue.Name = "cboxTrue";
+            this.cboxTrue.Size = new System.Drawing.Size(116, 33);
+            this.cboxTrue.TabIndex = 7;
+            this.cboxTrue.Text = "Правда";
+            this.cboxTrue.UseVisualStyleBackColor = false;
+            this.cboxTrue.CheckedChanged += new System.EventHandler(this.ChB_true_CheckedChanged);
+            // 
+            // оПрограммеToolStripMenuItem
+            // 
+            this.оПрограммеToolStripMenuItem.Name = "оПрограммеToolStripMenuItem";
+            this.оПрограммеToolStripMenuItem.Size = new System.Drawing.Size(94, 20);
+            this.оПрограммеToolStripMenuItem.Text = "О программе";
+            this.оПрограммеToolStripMenuItem.Click += new System.EventHandler(this.ОПрограммеToolStripMenuItem_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveAsToolStripMenuItem.Text = "Save as...";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.SaveAsToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::BelieveOrNotBelieve.Properties.Resources.Солнце;
+            this.ClientSize = new System.Drawing.Size(1020, 336);
+            this.Controls.Add(this.cboxTrue);
+            this.Controls.Add(this.nudNumber);
+            this.Controls.Add(this.btUdal);
+            this.Controls.Add(this.btSave);
+            this.Controls.Add(this.btDob);
+            this.Controls.Add(this.tboxQuestion);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ВерЮ НЕ ВерЮ";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumber)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.TextBox tboxQuestion;
+        private System.Windows.Forms.Button btDob;
+        private System.Windows.Forms.Button btSave;
+        private System.Windows.Forms.Button btUdal;
+        private System.Windows.Forms.NumericUpDown nudNumber;
+        private System.Windows.Forms.CheckBox cboxTrue;
+        private System.Windows.Forms.ToolStripMenuItem оПрограммеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+    }
+}
+
---
+++ BelieveOrNotBelieve/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//1. а) Создать приложение, показанное на уроке, добавив в него защиту от возможных ошибок(не создана база данных, обращение к несуществующему вопросу, открытие слишком большого файла и т.д.).
+//б) Изменить интерфейс программы, увеличив шрифт, поменяв цвет элементов и добавив другие «косметические» улучшения на свое усмотрение.
+//в) Добавить в приложение меню «О программе» с информацией о программе(автор, версия, авторские права и др.).
+//г) Добавить в приложение сообщение с предупреждением при попытке удалить вопрос.
+//д) Добавить пункт меню Save As, в котором можно выбрать имя для сохранения базы данных(элемент SaveFileDialog).
+//Владимр Гаврилов
+
+namespace BelieveOrNotBelieve
+{
+    public partial class Form1 : Form
+    {
+        // База данных с вопросами
+        TrueFalse database;
+
+        Stack<decimal> numbers = new Stack<decimal>();
+
+        public Form1()
+        {
+            InitializeComponent
+                (
+                );
+        }
+
+        private void FileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // Обработчик кнопки Добавить
+        private void BtDob_Click(object sender, EventArgs e)
+        {
+            if (database == null)
+            {
+                MessageBox.Show("Создайте новую базу данных", "Сообщение");
+                return;
+            }
+            database.Add((database.Count + 1).ToString(), true);
+            nudNumber.Maximum = database.Count;
+            nudNumber.Value = database.Count;
+        }
+
+        // Обработчик кнопки Удалить
+        private void BtUdal_Click(object sender, EventArgs e)
+        {
+
+            if (nudNumber.Maximum == 1 || database == null) return;
+            database.Remove((int)nudNumber.Value);
+            nudNumber.Maximum--;
+            if (nudNumber.Value > 1) nudNumber.Value = nudNumber.Value;
+            MessageBox.Show("Вопрос удален", "Сообщение");
+        }
+
+        // Обработчик кнопки Сохранить (вопрос)
+        private void BtSave_Click(object sender, EventArgs e)
+        {
+            try //защита от ошибки сохранения
+            {
+                database[(int)nudNumber.Value - 1].text = tboxQuestion.Text;
+                database[(int)nudNumber.Value - 1].trueFalse = cboxTrue.Checked;
+            }
+            catch { }
+        }
+
+        // Обработчик события изменения значения numericUpDown
+        private void NumericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            //Защита от обращения к несуществующему вопросу.
+            try
+            {
+                tboxQuestion.Text = database[(int)nudNumber.Value - 1].text;
+                cboxTrue.Checked = database[(int)nudNumber.Value - 1].trueFalse;
+            }
+            catch
+            {
+            }
+
+        }
+
+        private void ChB_true_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        // Обработчик пункта меню New
+        private void NewToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                database = new TrueFalse(sfd.FileName);
+                database.Add("123", true);
+                database.Save();
+                nudNumber.Minimum = 1;
+                nudNumber.Maximum = 1;
+                nudNumber.Value = 1;
+            }
+        }
+
+        // Обработчик пункта меню Open
+        private void OpenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+
+                try //Защита от загрузки файлов отличных от базы данных игры
+                {
+
+                    database = new TrueFalse(ofd.FileName);
+                    database.Load();
+                    nudNumber.Minimum = 1;
+                    nudNumber.Maximum = database.Count;
+                    nudNumber.Value = 1;
+                }
+                catch
+                {
+                    MessageBox.Show("Загрузите базу данных для игры или создайте новую", "Сообщение");
+                }
+
+            }
+
+        }
+
+        // Обработчик пункта меню Save
+        private void SaveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (database != null) database.Save();
+            else MessageBox.Show("База данных не создана");
+        }
+
+        // Обработчик пункта меню Exit
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void TBPlayPole_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ОПрограммеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Автор: Гаврилов Владимир  @GeekBrains", "Версия 1.1");
+        }
+
+        //д) Добавить пункт меню Save As, в котором можно выбрать имя для сохранения базы данных(элемент SaveFileDialog).
+        private void SaveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog ass = new SaveFileDialog();
+            ass.ShowDialog();
+            database = new TrueFalse(ass.FileName);
+            database.Add("123", true);
+            database.Save();
+            nudNumber.Minimum = 1;
+            nudNumber.Maximum = 1;
+            nudNumber.Value = 1;
+        }
+    }
+}
